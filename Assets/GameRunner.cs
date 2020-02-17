534e1426a2d6d6c94d93632c3fa5df391e60ddb0
Mark <unholybus@gmail.com>
2020-02-17 15:28:37 -0600

    Text Display For Baby

---
+++ Assets/GameRunner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameRunner : MonoBehaviour
+{
+
+    public int BabyCookies = 0;
+    public int TotalBabyCookies = 0;
+    public GameObject Text;
+    public string WinSendScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] objs;
+        objs = GameObject.FindGameObjectsWithTag("BabyCookie");
+        foreach (GameObject cookie in objs)
+        {
+            BabyCookies += 1;
+            TotalBabyCookies += 1;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text.GetComponent<Text>().text = BabyCookies.ToString() + "/" + TotalBabyCookies.ToString() + " Cookies Left To Save";
+        if (BabyCookies == 0)
+        {
+            SceneManager.LoadScene(WinSendScreen);
+        }
+    }
+}
